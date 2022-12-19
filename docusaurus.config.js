32d3b168c9ba74d1cac2e6b8f269f39226b07bd1
Marcos Jesús Santana Pérez <32165693+MarcosJSP@users.noreply.github.com>
2022-12-19 16:07:45 +0000

    feat: added tailwindcss (#2)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,6 +115,20 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+
+  plugins: [
+    async function tailwindcssPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    }
+  ],
 };
 
 module.exports = config;
