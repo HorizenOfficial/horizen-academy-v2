f7d518131c31dab020502689af5fdf1ca90b37cf
Marcos JSP - Laptop M1 Pro <marcos@theagilemonkeys.com>
2022-12-27 14:20:54 +0000

    feat: updated container padding to match the same padding we have on the core site

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
                 "horizen-blue": "#0E9DE5",
                 "horizen-green": "#26DB8D",
             },
+            container: {
+                padding: {
+                    DEFAULT: "1.5rem",
+                    md: "3rem",
+                    xl: "0.5rem",
+                },
+            },
         },
         fontFamily: {
             proxima: ["proxima-nova", "sans-serif"],
