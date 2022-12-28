7d9fabf4c91fafe236713e18af1b9dbc5274a785
Marcos JSP - Laptop M1 Pro <marcos@theagilemonkeys.com>
2022-12-28 12:38:03 +0000

    chore: updated barrel for all components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
+export { default as Button } from "./Button"
 export { default as HomepageFeatures } from "./HomepageFeatures"
-export { default as button } from "./button"
+export * from "./Newsletter"
