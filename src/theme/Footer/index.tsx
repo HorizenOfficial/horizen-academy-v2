aace7920402bc73a0e3335f39a48364bef8d74c7
Marcos JSP - Laptop M1 Pro <marcos@theagilemonkeys.com>
2022-12-30 11:31:18 +0000

    chore: added query client to footer so i can use react query

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,8 +1,15 @@
 import { Newsletter } from "@site/src/components"
 import React from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 const FooterWrapper = () => {
-    return <Newsletter />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Newsletter />
+        </QueryClientProvider>
+    )
 }
 
 export default FooterWrapper
