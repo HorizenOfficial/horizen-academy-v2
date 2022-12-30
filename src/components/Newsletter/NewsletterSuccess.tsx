73e1e4668d2517191764d0e02c6aa70f492c12d6
Marcos JSP - Laptop M1 Pro <marcos@theagilemonkeys.com>
2022-12-30 11:30:39 +0000

    feat: added newsletter success status component

---
+++ src/components/Newsletter/NewsletterSuccess.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const NewsletterSuccess = () => {
+    return (
+        <div className="flex-grow text-center">
+            <div className="text-white font-bold text-2xl">
+                Thank you for subscribing to our newest updates!
+            </div>
+            <div className="text-lg text-horizen-grey">
+                You will hear from us soon.
+            </div>
+        </div>
+    )
+}
+
+export default NewsletterSuccess
