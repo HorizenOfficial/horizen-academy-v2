3a1d0953685da51cc7aa9598bfddd8b8bc68075d
Marcos JSP - Laptop M1 Pro <marcos@theagilemonkeys.com>
2023-01-03 13:07:50 +0000

    chore: set the dark mode as the deafult mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,6 +114,10 @@ const config = {
                 theme: lightCodeTheme,
                 darkTheme: darkCodeTheme,
             },
+            colorMode: {
+                defaultMode: "dark",
+                disableSwitch: true,
+            },
         }),
 
     plugins: [
