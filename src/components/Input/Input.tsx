65d5a019b42a1a60933afbbc9dc29e8c093800bd
Marcos JSP - Laptop M1 Pro <marcos@theagilemonkeys.com>
2023-01-18 13:30:40 +0000

    fix: input component was not working properly

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,8 +3,17 @@ import clsx from "clsx"
 import React, { InputHTMLAttributes } from "react"
 import styles from "./Input.module.css"
 
-const Input: React.FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
-    return <input {...props} className={clsx(styles.input, props.className)} />
-}
+const Input = React.forwardRef<
+    HTMLInputElement,
+    InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
+    return (
+        <input
+            {...props}
+            className={clsx(styles.input, props.className)}
+            ref={ref}
+        />
+    )
+})
 
 export default Input
