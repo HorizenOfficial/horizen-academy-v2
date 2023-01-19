a423cbcbfdb80e746a33b5d7439188a8fae9e098
Marcos JSP - Laptop M1 Pro <marcos@theagilemonkeys.com>
2023-01-19 18:19:11 +0000

    fix: updated the name of the css file for the loader compoent

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import styles from "./Loader.styles.module.css"
+import styles from "./Loader.module.css"
 
 const Loader = () => {
     return <div className={styles.loader} />
