08a39875259955c87a72e4337c92e071f7c558d2
Marcos JSP - Laptop M1 Pro <marcos@theagilemonkeys.com>
2023-02-07 15:31:42 +0000

    feat: added images for each resolution

--- src/components/pages/homepage/HomepageHero/HomepageHero.tsx
+++ src/components/pages/homepage/HomepageHero/HomepageHero.tsx
@@ -1,18 +1,47 @@
 import React from "react"
 import useBaseUrl from "@docusaurus/useBaseUrl"
+import clsx from "clsx"
 import styles from "./HomepageHero.module.css"
 
 const HomepageHero = () => {
     return (
-        <header
-            className={styles.hero}
-            style={{
-                backgroundImage: `url('${useBaseUrl(
-                    "/assets/pages/homepage/hero-bg.jpg"
-                )}')`,
-            }}
-        >
-            <div className="container">
+        <header className={styles.hero}>
+            <div className={styles.hero__bg}>
+                <picture>
+                    <source
+                        srcSet={useBaseUrl(
+                            "/assets/pages/homepage/Academy-bckg_425x660.jpg"
+                        )}
+                        media="(max-width: 425px)"
+                    />
+                    <source
+                        srcSet={useBaseUrl(
+                            "/assets/pages/homepage/Academy-bckg_768x660.jpg"
+                        )}
+                        media="(max-width: 768px)"
+                    />
+                    <source
+                        srcSet={useBaseUrl(
+                            "/assets/pages/homepage/Academy-bckg_1024x660.jpg"
+                        )}
+                        media="(max-width: 1024px)"
+                    />
+                    <source
+                        srcSet={useBaseUrl(
+                            "/assets/pages/homepage/Academy-bckg_1440x660.jpg"
+                        )}
+                        media="(max-width: 1440px)"
+                    />
+                    <img
+                        src={useBaseUrl(
+                            "/assets/pages/homepage/Academy-bckg_2560x660.jpg"
+                        )}
+                        alt="hero background"
+                    />
+                </picture>
+            </div>
+
+            <div className={clsx("container", styles.hero__content)}>
                 <img
                     src={useBaseUrl(
                         "/assets/pages/homepage/hero-student-cap.svg"
