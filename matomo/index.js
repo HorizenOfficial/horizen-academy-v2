c57e5a8de84bb40a70b7a695374f7563caaf0e2d
Marcos Jesús Santana Pérez <32165693+MarcosJSP@users.noreply.github.com>
2023-02-09 13:40:02 +0000

    [MKTGTECH-273] Ensure New Analytics tags are included (#12)
    * feat: added matomo plugin
    
    * feat: test
    
    * feat: test 2
    
    * feat: test 3
    
    * feat: test 4
    
    * feat: test 5
    
    * feat:  only using analytics when node env is production

---
+++ matomo/index.js
@@ -0,0 +1,49 @@
+/* eslint-disable func-names */
+const path = require("path")
+
+module.exports = function (_context) {
+    const isProd = process.env.NODE_ENV === "production"
+
+    return {
+        name: "docusaurus-plugin-matomo",
+
+        getClientModules() {
+            return isProd ? [path.resolve(__dirname, "./track")] : []
+        },
+
+        injectHtmlTags() {
+            if (!isProd) {
+                return {}
+            }
+            return {
+                headTags: [
+                    {
+                        tagName: "link",
+                        attributes: {
+                            rel: "preconnect",
+                            href: `https://horizen.matomo.cloud/`,
+                        },
+                    },
+                    {
+                        tagName: "script",
+                        innerHTML: `
+                            var _paq = window._paq = window._paq || [];
+                            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                            _paq.push(["setCookieDomain", "*.horizen.io"]);
+                            _paq.push(["setDomains", ["*.horizen.io"]]);
+                            _paq.push(['trackPageView']);
+                            _paq.push(['enableLinkTracking']);
+                            (function () {
+                                var u = "https://horizen.matomo.cloud/";
+                                    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+                                _paq.push(['setSiteId', '1']);
+                                var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+                                g.async = true; g.src = '//cdn.matomo.cloud/horizen.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g, s);
+                            })();
+                        `,
+                    },
+                ],
+            }
+        },
+    }
+}
